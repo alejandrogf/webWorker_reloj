48752049ea2253f05d176403611602f5702d337e
Alejandrogf <alejandrogarciaf@gmail.com>
2015-10-06 13:58:34 +0200

    web worker reloj

--- webWorker_reloj/motor.js
+++ webWorker_reloj/motor.js
@@ -1,11 +1,30 @@
-﻿var worker = new worker("wwTemporizador.js");
+﻿var worker;
 
-worker.onmessage= function(evento) {
-
-    document.getElementById("segundos").innerHTML = evento.data;
-
-    if (evento.data == 0) { 
-        location.reload();
+function lanzarWorker() {
+    worker = new Worker("wwTemporizador.js");
+    worker.onmessage = function (evento) {
+        document.getElementById("segundos").innerHTML = evento.data;
+        if (evento.data == "0") {
+            location.reload();
+        }
     }
-
 }
+
+
+function parar() {
+    worker.terminate();
+    worker = undefined;
+}
+
+function reiniciar() {
+    if (worker == undefined) {
+        lanzarWorker();
+    } else {
+        worker.postMessage("reiniciar");
+    }
+    
+}
+
+lanzarWorker();
+document.getElementById("reiniciar").addEventListener("click",reiniciar);
+document.getElementById("stop").addEventListener("click", parar);
--- webWorker_reloj/wwTemporizador.js
+++ webWorker_reloj/wwTemporizador.js
@@ -5,8 +5,17 @@ function bajar() {
     segundos--;
 
     self.postMessage(segundos);
-
+    // Es una llamada recursiva. Se llama, dentro de si misma, 
+    // poniendo un intervalo de tiempo.
     setTimeout("bajar()", 1000);
 }
+
+self.onmessage=function(evento) {
+    if (evento.data == "reiniciar") {
+        segundos = 60;
+    }
+        
+}
+
 
 bajar();
